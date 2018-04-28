b8a33b05dca5bb7be25ebebafe2f77e2404ccb9e
Kevin R. Raney <kevin@raney.co>
2018-04-28 17:57:29 -0600

    remove noop bootstrap and remove ssh keys for now

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -87,44 +87,4 @@ class profile::base {
   group { 'web':
     ensure => 'present',
   }
-
-  ssh_authorized_key { 'kevin-iMac4k':
-    ensure => 'present',
-    name   => $title,
-    user   => 'kevin',
-    type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDlAAMXIwRLhnURnvIsjXkV5AawVgUR4yavk3Q6v008zgAiGiL7CTldpDNg9cwwaRT74kQmeEhYCfg3ZdHXcYL4RciAftvb/mS9rLXpEjCkIj//+h/J4L8kEBfrQt3cYr/fZsKUxj0taZNdWbLunnBJhDwpi707t1Fd6C5fcCA7VJvNHF8ewTQtdOYasgvNZVeA1/LWvLJKKZNbM2LA8ozq3/ZUjMmax1xCI3bpy9luybbQv7zqalWWBNue45LZXzFybm5MYik/lDAyk0zSEksKOaolYRW93I4L772WYcuFilzwvYvjLv01/L9MirLaMo5dfnDO6n99itbEEEKyL5WgCFXrzFICZzf5uy3MWBCcyR+wXboaPdzEUKtq0wyr+yhQi1gsyawSgvOdaWmDhVkAzPAFNawI+on0qTbgIKlPuCuOXwKnRXFnObjblZWDWdwGpv9SxutErrqVQtcWf+tRU3sWR9xuWFCfrCP7Mnvd3Xu48c6o3TFZIvNJWwzuJugJkZViYXFTHU9opBXmaLEGEFMFgGz7C+C1t+f6TtM/I+kGuiFWvdBqKaxHWnfUaqQLWu47WyhD0kiNlFCrt7dqVJ3QgdT4tt+LqNEzmHnWgDvE5mmshXrcokexmnlKTNlor6ABUOC4aGHtwQ0cplbMTNiWAEtUFk6Pyk+qOnGX2Q==',
-  }
-
-  ssh_authorized_key { 'kevin-iPhone-Working-Copy':
-    ensure => 'present',
-    name   => $title,
-    user   => 'kevin',
-    type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC909mvaFaJQMyHNPtyY61eU/1y0mTv9IKJk19gb9mgrxtMxew5wSB5D6LjadE1a5YeG7c0xqXqurUKSp1+R21PC3pbdqLR4j2L3930nAm7XVM+On8O54fv1QSZ0d/qyQz3eAIYRua8n1LILlBhwu9nzF9mp8siIOFaXT5LV6DCAVmBxRYzvS/unLVyyxCJtk6q7Tns/39LuUzMBHz/FaUsT/5TpvX7qxSwmJaIchjJEGKCiBL2EbgmRlwi8LbeoHat8htO2VtzYcLLUtS6gvWK1F7nAzSr3EQg2SipeC5NAl9tmedYIzR/9NBqzw4Lf/xnhSMSU7NiaEJ0Fn3mzwL7',
-  }
-
-  ssh_authorized_key { 'kevin-iPad-Working-Copy':
-    ensure => 'present',
-    name   => $title,
-    user   => 'kevin',
-    type   => 'ssh-rsa',
-    key    => '',
-  }
-
-  ssh_authorized_key { 'kevin-iPad-GFE-Working-Copy':
-    ensure => 'present',
-    name   => $title,
-    user   => 'kevin',
-    type   => 'ssh-rsa',
-    key    => '',
-  }
-
-  ssh_authorized_key { 'kevin-prompt':
-    ensure => 'present',
-    name   => $title,
-    user   => 'kevin',
-    type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAABJQAAAQEAgBx+vF1PcDwRaznKZMM0+ksDXv53NMTH+fcfvOQtvbz9pxkQusi9HJcdnry8rOampsjeSu5wMKeJZyxISA+TJKOTyvwF0N7xHSsYY3GvHtSNH3c+Q3a2d2AW1rcYZleDa8sSYVynaeY2drCdxWJUsvujNDXNk4lSBJp6pTCESpd4xIu9Oji5ZhxlWOWG0lmX3ybQAuw9tjGYi/B2B648ZwUx9U1y1ePi0i7/85qHIauY//+p8Nagayy3vApY0g1FVyrkdh2y3tqta/cgBQCedrY+ilFlsC/XcGSsCxfKkcPiZDKSmg03opxoXsxLc6IJJhlItYRPHG20v41RfM8bPQ==',
-  }
 }
