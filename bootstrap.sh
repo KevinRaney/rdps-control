fab0605816eddce3718009d2e5e1cdc1c1ce87bd
Kevin R. Raney <kevin@raney.co>
2018-06-02 18:50:29 -0600

    adjust bootstrap output to include the correct puppet command

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,16 +26,15 @@ echo "==> Installing modules from Puppetfile with r10k"
 r10k puppetfile install Puppetfile -v
 
 echo "==> Running Puppet apply"
-puppet apply manifests/site.pp \
-  --modulepath=./modules:./site \
-  --hiera_config=hiera.yaml
+puppet apply manifests/site.pp --modulepath=./modules:./site --hiera_config=hiera.yaml
 
-echo "========================================================================"
-echo "#          r10k needs to populate the Puppet environments              #"
-echo "========================================================================"
+echo "================================================================================================"
+echo "#          r10k needs to populate the Puppet environments                                      #"
+echo "================================================================================================"
 echo
 echo "  Do the following:"
+echo "    cd ~/control"
 echo "    r10k deploy environment -p -v"
-echo "    puppet agent -t"
+echo "    puppet apply manifests/site.pp --modulepath=./modules:./site --hiera_config=hiera.yaml"
 echo
-echo "========================================================================"
+echo "================================================================================================"
